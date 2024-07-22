22a1f1bd294378daeecb361d10fc179943af2edf
Dhurkesh-B <dhurkeshmyself@gmail.com>
2024-07-22 18:50:05 +0530

    version 1.0

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -137,7 +137,7 @@ const ColorButton = withStyles((theme) => ({
   },
 }))(Button);
 
-const apiUrl = "//3.110.197.31/predict";
+const apiUrl = "http://localhost:8000/predict";
 
 const ImageUpload = () => {
   const classes = useStyles();
